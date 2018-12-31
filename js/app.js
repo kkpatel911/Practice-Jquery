cdd6270d14f4f19c2ce26c8d332215f19b8a11b0
Kishan <kkpatel911@gmail.com>
2018-12-30 23:10:14 -0500

    first

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//1. Add a "featured" tag to the second item in the list: 
+  //  Append a new span element containing the word "Featured" to the list item
+  //  Add a class of "featured" to the new span element. 
+
+//2. Add a class of "new" to the last item in the list. You can traverse to the last item or find a jQuery method that helps you select the last item in the list. 
+
+//3. Add an attribute to each link so that all anchor tags open in a new tab. 
+
+//4. Select and print the index and text of each anchor tag to the console
+
+//5. Refactor question #4 using the 'this' keyword. 
